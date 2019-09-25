50752216500eb468c644ab0484fec402d2965c70
fx <792389209@qq.com>
2019-09-25 16:56:05 +0800

    账户信息-头像上传更新

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-card>
+  <el-card v-loading="loading">
     <bread-crumb slot="header">
       <template slot="title">账户信息</template>
     </bread-crumb>
@@ -26,30 +26,55 @@
         <el-button @click="saveUser" type="primary">保存信息</el-button>
       </el-form-item>
     </el-form>
-    <img class="head-img" :src="formData.photo?formData.photo:defaultImg" alt />
+    <el-upload action="" :show-file-list="false" :http-request="uploadImg">
+      <img class="head-img" :src="formData.photo?formData.photo:defaultImg" alt />
+    </el-upload>
   </el-card>
 </template>
 
 <script>
+import { log } from 'util'
 export default {
   data () {
     return {
+      loading: false,
       defaultImg: require('../../assets/img/404.png'),
-      formData: {
-      },
+      formData: {},
       accountRules: {
         name: [
           { required: true, message: '用户名不能为空' },
           { min: 1, max: 7, message: '用户名要控制在1到7个字符' }
         ],
-        email: [{ required: true, message: '邮箱不能为空' }, { pattern: /^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/, message: '邮箱格式不正确' }]
+        email: [
+          { required: true, message: '邮箱不能为空' },
+          {
+            pattern: /^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/,
+            message: '邮箱格式不正确'
+          }
+        ]
       }
     }
   },
   methods: {
+    uploadImg (params) {
+      console.log(params)
+      this.loading = true
+      let data = new FormData()
+      // 取出文件放到参数中
+      data.append('photo', params.file)
+      this.$axios({
+        url: '/user/photo',
+        method: 'patch',
+        data
+      }).then(result => {
+        // 成功上传的头像更新给当前的页面数据
+        this.formData.photo = result.data.photo
+        this.loading = false
+      })
+    },
     //   保存用户的个人信息
     saveUser () {
-      this.$refs.accountForm.validate((isOK) => {
+      this.$refs.accountForm.validate(isOK => {
         if (isOK) {
           this.$axios({
             url: '/user/profile',
@@ -64,10 +89,12 @@ export default {
     },
     //   获取用户的个人信息
     getUserInfo () {
+      this.loading = true
       this.$axios({
         url: '/user/profile'
       }).then(result => {
         this.formData = result.data
+        this.loading = false
       })
     }
   },
