4375cfb2356f69e5f5dc55712090b32130b0e839
fx <792389209@qq.com>
2019-09-26 21:18:26 +0800

    async和await新异步方案应用

---
+++ src/views/async/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-card>
+      <bread-crumb slot="header">
+        <template slot="title">异步应用</template>
+      </bread-crumb>
+      <p>{{count}}</p>
+      <el-button type="primary" @click="updateCountPromise">更新数据</el-button>
+      <el-button type="success" @click="newUpdate">更新数据</el-button>
+  </el-card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: 1
+    }
+  },
+  methods: {
+    //   async await
+    async newUpdate () {
+      let result = await this.updateAxios() // 造成强制等待
+      this.count = result
+    },
+    //   传统promise更新hjnb
+    async updateCountPromise () {
+      let result = await this.updateAxios()
+      this.count = result
+    },
+    updateAxios () {
+      return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+          resolve(2)
+        }, 1000)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
